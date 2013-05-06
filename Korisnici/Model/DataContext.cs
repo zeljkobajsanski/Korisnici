74c57f90c6f913dc78fa947fd27481794d572de1
Željko Bajšanski <zeljko.bajsanski@hotmail.com>
2013-05-06 15:28:35 +0200

    Context

---
+++ Korisnici/Model/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace mvc.com.Korisnici.Model
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Aplikacija> Aplikacije { get; set; }
+        public DbSet<Korisnik> Korisnici { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            var aplikacija = modelBuilder.Entity<Aplikacija>();
+            aplikacija.ToTable("Aplikacije");
+        }
+    }
+}
