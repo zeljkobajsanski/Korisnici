43eda59613636fb4dcba6b82a43d9d3e4fcd19e4
Željko Bajšanski <zeljko.bajsanski@hotmail.com>
2013-05-06 15:21:42 +0200

    Korisnik

---
+++ Korisnici/Model/Korisnik.cs
@@ -0,0 +1,10 @@
+﻿namespace mvc.com.Korisnici.Model
+{
+    public class Korisnik : Entity
+    {
+        public string KorisnickoIme { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string EMail { get; set; }
+    }
+}
