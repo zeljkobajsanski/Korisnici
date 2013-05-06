a87d17d93107dcb8529bd8245a8c7e0ab4858a3b
Željko Bajšanski <zeljko.bajsanski@hotmail.com>
2013-05-06 15:45:05 +0200

    bla bla

--- Korisnici/Model/Aplikacija.cs
+++ Korisnici/Model/Aplikacija.cs
@@ -1,7 +1,14 @@
-﻿namespace mvc.com.Korisnici.Model
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvc.com.Korisnici.Model
 {
     public class Aplikacija : Entity
     {
+        [Required(ErrorMessage = "Naziv aplikacije nije unet")]
+        [StringLength(255, ErrorMessage = "Dozvoljena dužina naziva aplikacije je do 255 karaktera")]
         public string Naziv { get; set; }
+
+        public IList<Korisnik> Korisnici { get; set; }
     }
 }
--- Korisnici/Model/Korisnik.cs
+++ Korisnici/Model/Korisnik.cs
@@ -1,10 +1,31 @@
-﻿namespace mvc.com.Korisnici.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvc.com.Korisnici.Model
 {
     public class Korisnik : Entity
     {
+        [Required(ErrorMessage = "Korisničko ime nije uneto")]
+        [StringLength(50, ErrorMessage = "Dozvoljena dužina korisničkog imena je do 25 karaktera")]
         public string KorisnickoIme { get; set; }
+
+        [Required(ErrorMessage = "Ime nije uneto")]
+        [StringLength(50, ErrorMessage = "Dozvoljena dužina je do 25 karaktera")]
         public string Ime { get; set; }
+        
+        [Required(ErrorMessage = "Prezime nije uneto")]
+        [StringLength(50, ErrorMessage = "Dozvoljena dužina je do 25 karaktera")]
         public string Prezime { get; set; }
+
+        [Required(ErrorMessage = "E-mail nije unet")]
+        [StringLength(255, ErrorMessage = "Dozvoljena dužina je do 255 karaktera")]
+        [RegularExpression(@"^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$", ErrorMessage = "E-mail je neispravan")]
         public string EMail { get; set; }
+
+        [Required(ErrorMessage = "Lozinka nije uneta")]
+        public byte[] Lozinka { get; set; }
+
+        public Aplikacija Aplikacija { get; set; }
+
+        public int AplikacijaId { get; set; }
     }
 }
